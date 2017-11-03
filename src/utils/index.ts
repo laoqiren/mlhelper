17cc19a8e3a739c2bad20ff4b9f1dacac6047111
laoqiren <780649403@qq.com>
2017-11-03 22:20:00 +0800

    fix #1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import Matrix from './matrix/index';
 import Vector from './vector/index';
-import fileParser from './fileParser/index';
-import charts from './charts/index';
+import * as fileParser from './fileParser/index';
+import * as charts from './charts/index';
 
 export default {
     Matrix,
