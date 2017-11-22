f1a8162554b8268ac87a7a0c50b4673dc84ee278
laoqiren <780649403@qq.com>
2017-11-22 23:10:12 +0800

    vector documentation

--- src/utils/vector/index.ts
+++ src/utils/vector/index.ts
@@ -4,6 +4,12 @@ import * as _ from 'lodash';
 class Vector {
     constructor(public arr: Array<number>){
     }
+    /**
+     * 数组元素从小到大排序对应的下标 The sorted index of array.
+     * 
+     * @returns {number[]} 
+     * @memberof Vector
+     */
     argSort(): number[]{
         let list = [...this.arr];
         let result = list
@@ -18,6 +24,14 @@ class Vector {
       //  console.log(result)
         return result;
     }
+    /**
+     * 针对每个元素，若值等于0返回0，若大于0返回1，若小于0返回-1   For each element, when its value equals to 0 returns 0, else if it's larger than 0 returns 1 else returns -1.
+     * 
+     * @static
+     * @param {(number|Array<number>)} arr 
+     * @returns {(number|Array<number>)} 
+     * @memberof Vector
+     */
     static sign(arr: number|Array<number>): number|Array<number>{
         if(Array.isArray(arr)){
             return arr.map(v=>v===0.0?0.0:(v>0.0?1.0:-1.0))
@@ -25,6 +39,14 @@ class Vector {
             return arr===0.0?0.0:(arr>0.0?1.0:-1.0);
         }
     }
+    /**
+     * 创建指定个数的0-1之间的随机数  Create specific number of random number between 0 and 1.
+     * 
+     * @static
+     * @param {number} m 
+     * @returns 
+     * @memberof Vector
+     */
     static rand(m: number){
         let initArr = Repeat(0,m).toArray();
         let result = initArr.map(v=>Math.random());
