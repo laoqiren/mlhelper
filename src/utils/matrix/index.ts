499ecc62b0ec46daaad41d7150eee70576abff0a
laoqiren <780649403@qq.com>
2017-11-22 22:30:17 +0800

    matrix utils documentation

--- src/utils/matrix/index.ts
+++ src/utils/matrix/index.ts
@@ -5,16 +5,22 @@ import * as _ from 'lodash';
 class Matrix {
     constructor(public arr: Array<Array<number>>){}
     
+    /**
+     * 获取原始多维数组 Get raw data.
+     * 
+     * @returns {Array<Array<number>>} 
+     * @memberof Matrix
+     */
     toArray(): Array<Array<number>>{
         return this.arr;
     }
     /**
-     * 针对两个矩阵同一行同一列的值对应进行计算
+     * 针对两个矩阵同一行同一列的值对应进行计算 Matrix operation. like '*','/','+','-'.
      * 
-     * @param {Class Matrix} arrA 矩阵A
-     * @param {Class Matrix} arrB 矩阵B
-     * @param {String} operator  操作，'+'|'-'|'*'|'\'
-     * @returns {Class Matrix} 计算后的矩阵
+     * @param {Class Matrix} arrA Matrix A
+     * @param {Class Matrix} arrB Matrix B
+     * @param {String} operator  operation，'+'|'-'|'*'|'\'
+     * @returns {Class Matrix} result.
      * @memberof Matrix
      */
     zipWith(arrA: Array<Array<number>>,arrB: Array<Array<number>>,operator: string): Matrix{
@@ -59,60 +65,102 @@ class Matrix {
         }
         return new Matrix(result);
     }
+    /**
+     * Matrix subtraction
+     * 
+     * @param {Matrix} toSub Matrix to sub with.
+     * @returns {Matrix} 
+     * @memberof Matrix
+     */
     sub(toSub: Matrix): Matrix{
         return this.zipWith(this.arr,toSub.arr,'-');
     }
+    /**
+     * matrix addition
+     * 
+     * @param {Matrix} toAdd Matrix to add with.
+     * @returns {Matrix} 
+     * @memberof Matrix
+     */
     add(toAdd: Matrix): Matrix{
         return this.zipWith(this.arr,toAdd.arr,'+')
     }
+    /**
+     * matrix multiplication
+     * 
+     * @param {Matrix} toMult matrix to multiply with.
+     * @returns {Matrix} 
+     * @memberof Matrix
+     */
     mult(toMult: Matrix): Matrix{
         return this.zipWith(this.arr,toMult.arr,'*')
     }
+    /**
+     * Matrix Division
+     * 
+     * @param {Matrix} toDivide matrix to divide with.
+     * @returns {Matrix} 
+     * @memberof Matrix
+     */
     divide(toDivide: Matrix): Matrix{
         return this.zipWith(this.arr,toDivide.arr,'/');
     }
+    /**
+     * Get the size of Matrix, including rows and columns.
+     * 
+     * @returns {[number,number]} 
+     * @memberof Matrix
+     */
     size(): [number,number]{
         return [this.arr.length,this.arr[0].length];
     }
     /**
-     * 矩阵同一行进行相加
+     * 矩阵同一行/列进行相加
      * 
      * @param {number} [axis=1] 
      * @returns {Array}
      * @memberof Matrix
      */
-    sum(axis?: number): Array<number>{
-        let result = [];
-        for(let row of this.arr){
-            let rowSum = row.reduce((pre,cur)=>pre+cur);
-            result.push(rowSum);
+    sum(axis=1 as number): Array<number>{
+        if(axis === 0){
+            return this.transpose().map(v=>_.sum(v));
         }
-        return result;
+        return this.arr.map(v=>_.sum(v));
     }
-    // 矩阵每一列的最小值
-    min(axis?: number): Array<number>{
-        let cols = this.arr[0].length;
-        let result = [];
+    /**
+     * 
+     * 获取同一行/列的最小值
+     * @param {number} [axis=0 as number] 
+     * @returns {Array<number>} 
+     * @memberof Matrix
+     */
+    min(axis=0 as number): Array<number>{
+        let arr = axis === 0?this.transpose():[...this.arr];
+
+        arr = arr.map(v=>v.filter(c=>typeof c === 'number'));
+
+        return arr.map(v=>_.min(v));
+    }
+    /**
+     * 获取同一行/列的最大值
+     * 
+     * @param {number} [axis=0 as number] 
+     * @returns {Array<number>} 
+     * @memberof Matrix
+     */
+    max(axis=0 as number): Array<number>{
+        let arr = axis === 0?this.transpose():[...this.arr];
         
-        for(let i=0; i<cols; i++){
-            let col = this.arr.map(v=>v[i]);
-            col = col.filter(v=> typeof v === 'number')
-            let colMin = Math.min(...col);
-            result.push(colMin);
-        }
-        return result;
+        arr = arr.map(v=>v.filter(c=>typeof c === 'number'));
+
+        return arr.map(v=>_.max(v));
     }
-    max(axis?: number): Array<number>{
-        let cols = this.arr[0].length;
-        let result = [];
-        for(let i=0; i<cols; i++){
-            let col = this.arr.map(v=>v[i]);
-            col = col.filter(v=> typeof v === 'number')
-            let colMax = Math.max(...col);
-            result.push(colMax);
-        }
-        return result;
-    }
+    /**
+     * 转置矩阵
+     * 
+     * @returns {Array<Array<number>>} 
+     * @memberof Matrix
+     */
     transpose(): Array<Array<number>>{
         return math.transpose(this.arr);
     }
@@ -130,6 +178,15 @@ class Matrix {
     static ones(m: number,n?: number){
         return n?math.ones(m,n)._data:math.ones(m)._data;
     }
+    /**
+     * 获取同一行或同一列的平均值
+     * 
+     * @static
+     * @param {Array<Array<number>>} arr 
+     * @param {number} [axis=0 as number] 
+     * @returns {Array<number>} 
+     * @memberof Matrix
+     */
     static mean(arr: Array<Array<number>>, axis=0 as number): Array<number>{
         
         if(axis === 0){ //按列求平均值
---
+++ tt.js
@@ -0,0 +1,3 @@
+const math = require('mathjs');
+
+console.log(math.transpose([[1,3],[2,6]])[0])
